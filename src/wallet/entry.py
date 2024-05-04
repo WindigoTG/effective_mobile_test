6b51719ebfffb6b2130395251effa602f0945827
Maxim Olesov <windigotp@gmail.com>
2024-05-04 17:03:50 +0800

    WalletEntry class added

---
+++ src/wallet/entry.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+import datetime
+from enum import IntEnum
+
+
+class EntryCategory(IntEnum):
+    """ Перечисление категорий записи. """
+    Income = 1
+    Spend = 2
+
+
+CATEGORY = {
+    EntryCategory.Income: "Доход",
+    EntryCategory.Spend: "Расход",
+}
+
+
+@dataclass
+class WalletEntry:
+    """ Класс, представляющий запись в кошельке. """
+    date: datetime.date
+    category: EntryCategory
+    amount: float
+    description: str
+
+    def __str__(self):
+        return "Дата: {date}\nКатегория: {cat}\nСумма: {amt}\n" \
+               "Описание:{desc}".format(
+                    date=self.date,
+                    cat=CATEGORY[self.category],
+                    amt=round(self.amount, 2),
+                    desc=self.description,
+                )
+
+    def to_json(self):
+        return {
+            "date": self.date.isoformat(),
+            "category": self.category.value,
+            "amount": round(self.amount, 2),
+            "description": self.description,
+        }
